927a3ec908b898fcf2c53f5ea96084d32bb30a8a
MewW6m <aheadlink.changat@gmail.com>
2023-11-03 14:23:05 +0900

    Delete generalList_rails directory

--- generalList_rails/src/app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  #has_many :stock, foreign_key "registUser"
-  #has_many :stock, foreign_key "updateUser"
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
