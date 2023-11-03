310d47df9b942d9d93dd1712ecdba60f185122d0
MewW6m <aheadlink.changat@gmail.com>
2023-11-03 14:12:23 +0900

    Delete generalList_Rails directory

--- generalList_Rails/src/app/models/stock.rb
+++
@@ -1,4 +0,0 @@
-class Stock < ApplicationRecord
-  #belongs_to :user, foreign_key "registUser"
-  #belongs_to :user, foreign_key "updateUser"
-end
--- generalList_Rails/src/db/migrate/20221021070059_change_item_code_to_stocks.rb
+++
@@ -1,7 +0,0 @@
-class ChangeItemCodeToStocks < ActiveRecord::Migration[7.0]
-  def change
-    change_column :stocks, :itemCode, :bigint
-    change_column :stocks, :registUser, :bigint
-    change_column :stocks, :updateUser, :bigint
-  end
-end
